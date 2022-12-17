b7c6d4484db07cd3005bc59aa459c509b4c03cd8
nirlepshah <nirlepshah@gmail.com>
2022-12-16 23:05:55 -0500

    commit

--- index.js
+++ index.js
@@ -22,21 +22,22 @@ const port = process.env.PORT || 3000;
 const app = express()
 
 
-var allowedOrigins = ['http://localhost:8080',
-    'http://yourapp.com'];
+app.use(cors())
+// var allowedOrigins = ['http://localhost:8080',
+//     'http://yourapp.com'];
 
-app.use(cors({
-    origin: function (origin, callback) {
+// app.use(cors({
+//     origin: function (origin, callback) {
 
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 
 
