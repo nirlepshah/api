7e6d343c9643904ad400fe01c73ab2a0f5fb9b3b
nirlepshah <nirlepshah@gmail.com>
2022-12-26 19:15:09 -0500

    commit

--- models.js
+++ models.js
@@ -25,7 +25,7 @@ let userSchema = mongoose.Schema({
     Username: { type: String, required: true },
     Password: { type: String, required: true },
     Email: { type: String, required: true },
-    Birthday: { type: Date, required: true },
+    Birthdate: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 })
 
